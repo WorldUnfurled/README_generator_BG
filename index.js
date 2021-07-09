1e2e5404c2c2af69bfaa7257947a57f9600d4282
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-07-09 18:34:07 -0400

    Added line space between Header and relevant text; Added bullet points to Email and Github under questions

--- index.js
+++ index.js
@@ -52,14 +52,14 @@ inquirer
             message: "What is the Github profile for this project's repo?"
         }
     ]).then((data) => {
-        const title = `# ` + `${headers[0]}` + `\n` + `${data.title}` + `\n\n`;
-        const description = `## ` + `${headers[1]}` + `\n` + `${data.description}` + `\n\n`;
-        const installation = `## ` + `${headers[2]}` + `\n` + `${data.installation}` + `\n\n`;
-        const usage = `## ` + `${headers[3]}` + `\n` + `${data.usage}` + `\n\n`;
-        const contributing = `## ` + `${headers[4]}` + `\n` + `${data.contributing}` + `\n\n`;
-        const tests = `## ` + `${headers[5]}` + `\n` + `${data.tests}` + `\n\n`;
-        const license = `## ` + `${headers[6]}` + `\n` + `${data.license}` + `\n\n`;
-        const questions = `## ` + `${headers[7]}` + `\n` + `${data.qEmail}` + `\n` + `${data.qGithub}` + `\n\n`;
+        const title = `# ` + `${headers[0]}` + `\n\n` + `${data.title}` + `\n\n`;
+        const description = `## ` + `${headers[1]}` + `\n\n` + `${data.description}` + `\n\n`;
+        const installation = `## ` + `${headers[2]}` + `\n\n` + `${data.installation}` + `\n\n`;
+        const usage = `## ` + `${headers[3]}` + `\n\n` + `${data.usage}` + `\n\n`;
+        const contributing = `## ` + `${headers[4]}` + `\n\n` + `${data.contributing}` + `\n\n`;
+        const tests = `## ` + `${headers[5]}` + `\n\n` + `${data.tests}` + `\n\n`;
+        const license = `## ` + `${headers[6]}` + `\n\n` + `${data.license}` + `\n\n`;
+        const questions = `## ` + `${headers[7]}` + `\n\n` + `* ` + `${data.qEmail}` + `\n` + `* ` + `${data.qGithub}` + `\n\n`;
 
         readmeText = [];
         readmeText.push(title);
@@ -79,5 +79,4 @@ inquirer
         fs.writeFile('sampleREADME.md', readmeGen, (err) => {
             err ? console.error(err) : console.log('Commit logged!')
         });
-        console.log(data.title);
     });
